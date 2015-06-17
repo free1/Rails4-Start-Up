120a91d234a030f5db52a6f6cb887fdec78e0fc4
free1 <747549945@qq.com>
2015-06-17 15:35:14 +0800

    cc

--- app/api/v1/entities/products.rb
+++ app/api/v1/entities/products.rb
@@ -9,7 +9,9 @@ module V1
 
     class ProductDetail < Grape::Entity
     	expose :id, :title, :location, :show_time, :price, :watch_count, :describe
-    	# expose :cover_path 
+    	expose :cover_path do |product|
+    		"http://101.200.235.120#{product.cover_path}"
+    	end
     end
   end
 end
